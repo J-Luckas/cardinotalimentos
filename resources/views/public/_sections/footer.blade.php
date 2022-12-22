eae74e93ba273ee31207d7058cf0e84b46faaca2
Jairo Luckas <jairoluckas@hotmail.com>
2022-12-22 13:25:34 -0300

    feat: add feliz-natal 2022 cheers

--- resources/views/public/_sections/footer.blade.php
+++ resources/views/public/_sections/footer.blade.php
@@ -24,9 +24,7 @@
         <!-- jquery -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
         <script type="module">
-
-
-            window.onload = (event) => {
+            window.addEventListener("load", ( event ) =>  {
                 var currentDate = new Date();
                 var christmasDate = new Date( `December 25, ${currentDate.getFullYear()} 23:59:59` );
                 const getCookie = function (name) {
@@ -36,10 +34,10 @@
                 }
 
 
-
+                // console.log('teste: ', christmasDate);
                 if( currentDate <= christmasDate && !getCookie("modalChristmas")){
                     Swal.fire({
-                        imageUrl: window.location.origin + '/img/feliz-natal.jpg',
+                        imageUrl: window.location.origin + '/img/feliz-natal.png',
                         showCloseButton: true,
                         focusConfirm: false,
                         showConfirmButton: false,
@@ -57,40 +55,7 @@
                     document.cookie =  "modalChristmas=true;" + expires + ";path=/";
                 }
 
-            };
-
-            window.onload = (event) => {
-                var currentDate = new Date();
-                var newYearDate = new Date( `January 01, ${currentDate.getFullYear()} 23:59:59` );
-                const getCookie = function (name) {
-                    const value = `; ${document.cookie}`;
-                    const parts = value.split(`; ${name}=`);
-                    if (parts.length === 2) return parts.pop().split(';').shift();
-                }
-
-
-
-                if( currentDate <= newYearDate && !getCookie("modalNewYear")){
-                    Swal.fire({
-                        imageUrl: window.location.origin + '/img/feliz-natal.jpg',
-                        showCloseButton: true,
-                        focusConfirm: false,
-                        showConfirmButton: false,
-                        imageWidth: 400,
-                        imageHeight: 400,
-                        imageAlt: 'Feliz Natal!',
-                        backdrop: `
-                            rgb(176, 28, 34,0.4)
-                        `
-                    });
-
-                    let dateExp = new Date();
-                    dateExp.setTime(dateExp.getTime() + ( 30 * 60 * 1000));
-                    let expires = "expires="+dateExp.toUTCString();
-                    document.cookie =  "modalNewYear=true;" + expires + ";path=/";
-                }
-
-            };
+            });
 
         </script>
     </body>
