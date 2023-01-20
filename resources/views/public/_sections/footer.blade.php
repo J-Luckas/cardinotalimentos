9795a71f0375804891fb3ecda528a94624c3c3d1
Jairo Luckas <jairoluckas@hotmail.com>
2023-01-20 15:07:43 -0300

    feature: remove merry

--- resources/views/public/_sections/footer.blade.php
+++ resources/views/public/_sections/footer.blade.php
@@ -24,36 +24,36 @@
         <!-- jquery -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
         <script type="module">
-            window.addEventListener("load", ( event ) =>  {
-                var currentDate = new Date();
-                var christmasDate = new Date( `December 25, ${currentDate.getFullYear()} 23:59:59` );
-                const getCookie = function (name) {
-                    const value = `; ${document.cookie}`;
-                    const parts = value.split(`; ${name}=`);
-                    if (parts.length === 2) return parts.pop().split(';').shift();
-                }
+            // window.addEventListener("load", ( event ) =>  {
+            //     var currentDate = new Date();
+            //     var christmasDate = new Date( `December 25, ${currentDate.getFullYear()} 23:59:59` );
+            //     const getCookie = function (name) {
+            //         const value = `; ${document.cookie}`;
+            //         const parts = value.split(`; ${name}=`);
+            //         if (parts.length === 2) return parts.pop().split(';').shift();
+            //     }
 
-                if( currentDate <= christmasDate && !getCookie("modalChristmas")){
-                    Swal.fire({
-                        imageUrl: window.location.origin + '/img/feliz-natal.png',
-                        showCloseButton: true,
-                        focusConfirm: false,
-                        showConfirmButton: false,
-                        imageWidth: 400,
-                        imageHeight: 400,
-                        imageAlt: 'Feliz Natal!',
-                        backdrop: `
-                            rgb(176, 28, 34,0.4)
-                        `
-                    });
+            //     if( currentDate <= christmasDate && !getCookie("modalChristmas")){
+            //         Swal.fire({
+            //             imageUrl: window.location.origin + '/img/feliz-natal.png',
+            //             showCloseButton: true,
+            //             focusConfirm: false,
+            //             showConfirmButton: false,
+            //             imageWidth: 400,
+            //             imageHeight: 400,
+            //             imageAlt: 'Feliz Natal!',
+            //             backdrop: `
+            //                 rgb(176, 28, 34,0.4)
+            //             `
+            //         });
 
-                    let dateExp = new Date();
-                    dateExp.setTime(dateExp.getTime() + ( 30 * 60 * 1000));
-                    let expires = "expires="+dateExp.toUTCString();
-                    document.cookie =  "modalChristmas=true;" + expires + ";path=/";
-                }
+            //         let dateExp = new Date();
+            //         dateExp.setTime(dateExp.getTime() + ( 30 * 60 * 1000));
+            //         let expires = "expires="+dateExp.toUTCString();
+            //         document.cookie =  "modalChristmas=true;" + expires + ";path=/";
+            //     }
 
-            });
+            // });
 
         </script>
     </body>
