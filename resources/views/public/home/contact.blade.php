3c9c8aa03f46a0fc968e64fc7599c5330db59261
J-Luckas <jairoluckas@hotmail.com>
2023-11-01 15:09:10 -0300

    feat: adiciona botão de email

--- resources/views/public/home/contact.blade.php
+++ resources/views/public/home/contact.blade.php
@@ -6,34 +6,6 @@
     </div>
     <div class="row">
         <div class="col-lg-6">
-            <div class="msg-ret-contact slideanim">
-            </div>
-            <form>
-                <div class="form-group slideanim">
-                    <label for="nome">Nome</label>
-                    <input type="text" class="form-control" id="nome" name="nome" placeholder="Insira seu nome">
-                </div>
-                <div class="form-group slideanim">
-                    <label for="email">Email</label>
-                    <input type="email" class="form-control" id="email" name="email" placeholder="Insira seu email">
-                </div>
-                <div class="form-group slideanim">
-                    <label for="telefone">Telefone</label>
-                    <input type="text" class="form-control" id="telefone" name="telefone" placeholder="Insira seu telefone">
-                </div>
-
-                <div class="form-group slideanim">
-                    <label for="mensagem">Mensagem</label>
-                    <textarea rows="5" class="form-control" name="mensagem" id="mensagem" style="resize: none;" placeholder=""></textarea>
-
-                </div>
-                <button style="width: 150px;"  type="button" id="enviar-email" class="btn btn-primary">
-                    <span id="send-email-button">
-                        Enviar
-                    </span>
-                    <img style="display:none;" id="load-email" src="{{asset("img/contact-loader.gif")}}" alt="">
-                </button>
-            </form>
         </div>
         <div class="col-lg-6">
             <ul class="lista-contato">
@@ -56,6 +28,14 @@
                     <div class="div-social-ul soc-3 slideanim">
                         <a href="https://www.instagram.com/cardinotalimentos/" target="_blank">
                             <i class="bi bi-instagram social-media-icon px-3"></i>Instagram
+                        </a>
+                    </div>
+                </li>
+                <li>
+
+                    <div class="div-social-ul soc-4 slideanim">
+                        <a href = "mailto: contato@cardinotalimentos.com">
+                            <i class="bi bi-envelope-at social-media-icon px-3"></i>E-mail
                         </a>
                     </div>
                 </li>
